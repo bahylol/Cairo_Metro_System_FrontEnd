1f8a7b0bb8e824f3da2ded55fadffbf6431921ce
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-05-31 19:37:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Navbar from './pages/Navbar/Navbar2.js';
+// import Navbar from './pages/Navbar/Navbar2.js';
 // import Footer from './pages/Footer/Footer.js';
-import Home from './pages/Home/Home.js';
+// import Home from './pages/Home/Home.js';
 // import Signup from './pages/Signup-Login/Signup';
 // import Login from './pages/Signup-Login/Login';
 import Refund_Request_Page from './pages/Refund_Request/Refund_Request.js';
@@ -11,7 +11,6 @@ function App() {
 	return (
 		<div className="App">
 			<div className="Navbar">
-				<Navbar />
 				<div className="content">
 					<Refund_Request_Page />
 				</div>

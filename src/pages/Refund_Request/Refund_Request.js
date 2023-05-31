3d367dae30d1cd287e62f6819aabf55149d5bce0
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-05-31 10:50:43 +0300

    working on RR page

--- src/pages/Refund_Request/Refund_Request.js
+++ src/pages/Refund_Request/Refund_Request.js
@@ -1,11 +1,44 @@
-import './Refund_Request.css';
+import './RR.css';
+import { useEffect, useState } from 'react';
 
 const Refund_Request = () => {
+	const [requests, setRequests] = useState([
+		{ title: 'Request 1', body: 'lore ipsum...', author: 'mario', id: 1 },
+		{ title: 'Request 2', body: 'lore ipsum...', author: 'Luigi', id: 2 },
+		{ title: 'Request 3', body: 'lore ipsum...', author: 'Joe', id: 3 },
+	]);
+
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		try {
+			const response = await fetch('http://localhost:3000/api/v1/users/refund_request/', {
+				method: 'POST',
+			});
+			const jsonData = await response.json();
+			setData(jsonData);
+		} catch (error) {
+			console.error('Error fetching data!!!:', error);
+		}
+	};
+
 	return (
 		<section className="RRcontainer">
-			<header>Login Form</header>
+			<header>Your Requests</header>
+
+			{requests.map((request) => (
+				<div className="Request Preview" key="request.id">
+					<h2>{request.title}</h2>
+					<p>Written by {request.author}</p>
+				</div>
+			))}
+
 			<form action="#" className="RRform">
-				<div className="RRinput-box">
+				{/* <div className="RRinput-box">
 					<label className="labels">Email</label>
 					<input type="text" placeholder="Enter email" required />
 				</div>
@@ -13,9 +46,9 @@ const Refund_Request = () => {
 				<div className="RRinput-box">
 					<label className="labels">Password</label>
 					<input type="password" placeholder="Enter password" required />
-				</div>
+				</div> */}
 
-				<button>Log in</button>
+				{/* <button>Log in</button> */}
 			</form>
 		</section>
 	);

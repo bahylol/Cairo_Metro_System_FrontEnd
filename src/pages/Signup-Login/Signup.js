831f76dd7cf14b9d7331ecc6cb9b290db5255c9d
bahylol <bahymohamed2010@gmail.com>
2023-06-01 14:40:52 +0300

    Update Signup.js

--- src/pages/Signup-Login/Signup.js
+++ src/pages/Signup-Login/Signup.js
@@ -29,7 +29,7 @@ function RegistrationForm() {
 	};
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		if(user.password !==user.confirmPassword){
+		if (user.password !== user.confirmPassword) {
 			alert("passwords don't match");
 			return;
 		}
@@ -56,83 +56,85 @@ function RegistrationForm() {
 
 
 	return (
-		<section className="SL-Page">
-			<header>Signup Form</header>
-			<form action="#" className="SL-form">
+		<div className="SL-Page">
+			<section className="SL-container">
+				<header>Signup Form</header>
+				<form action="#" className="SL-form">
 
-				<div className="SL-column">
-					<div className="SL-input-box">
-						<label>Full Name</label>
-						<input type="text" placeholder="Enter full name" required />
-					</div>
-
-					<div className="SL-input-box">
-						<label>Username</label>
-						<input type="text" placeholder="Enter Username" name="username" value={user.username} onChange={handleChange} required />
-					</div>
-				</div>
-
-				<div className="SL-input-box">
-					<label>Email Address</label>
-					<input type="text" placeholder="Enter email address" name="email" value={user.email} onChange={handleChange} required />
-				</div>
-
-				<div className="SL-input-box">
-					<label>Password</label>
-					<input type="password" placeholder="Enter password" name="password" value={user.password} onChange={handleChange} required />
-				</div>
-
-				<div className="SL-input-box">
-					<label>Confirm Password</label>
-					<input type="password" placeholder="Enter password again" name="confirmPassword" value={user.confirmPassword} onChange={handleChange} required />
-				</div>
-
-				<div className="SL-column">
-					<div className="SL-input-box">
-						<label>Phone Number</label>
-						<input type="number" placeholder="Enter phone number" name="phone" value={user.phone} onChange={handleChange} required />
-					</div>
-					<div className="SL-input-box">
-						<label>Birth Date</label>
-						<input type="date" placeholder="Enter birth date" name="date" value={user.date} onChange={handleChange} required />
-					</div>
-				</div>
-
-				<div className="SL-gender-box">
-					<h3>Gender</h3>
-					<div className="SL-gender-option">
-						<div className="SL-gender">
-							<input type="radio" id="check-male" name="gender"
-								value="M"
-								checked={gender === "M"}
-								onChange={handleRadio} />
-							<label htmlFor="check-male">Male</label>
+					<div className="SL-column">
+						<div className="SL-input-box">
+							<label>Full Name</label>
+							<input type="text" placeholder="Enter full name" required />
 						</div>
-						<div className="SL-gender">
-							<input type="radio" id="check-female" name="gender"
-								value="F"
-								checked={gender === "F"}
-								onChange={handleRadio} />
-							<label htmlFor="check-female">Female</label>
-						</div>
-						<div className="SL-gender">
-							<input type="radio" id="check-other" name="gender"
-								value="P"
-								checked={gender === "P"}
-								onChange={handleRadio} />
-							<label htmlFor="check-other">prefer not to say</label>
+
+						<div className="SL-input-box">
+							<label>Username</label>
+							<input type="text" placeholder="Enter Username" name="username" value={user.username} onChange={handleChange} required />
 						</div>
 					</div>
-				</div>
 
-				<div className="SL-input-box">
-					<label>Social Secuirty Number</label>
-					<input type="number" placeholder="Enter SSN" name="ssn" value={user.ssn} onChange={handleChange} required />
-				</div>
+					<div className="SL-input-box">
+						<label>Email Address</label>
+						<input type="text" placeholder="Enter email address" name="email" value={user.email} onChange={handleChange} required />
+					</div>
 
-				<button onClick={handleSubmit}>Sign up</button>
-			</form>
-		</section>
+					<div className="SL-input-box">
+						<label>Password</label>
+						<input type="password" placeholder="Enter password" name="password" value={user.password} onChange={handleChange} required />
+					</div>
+
+					<div className="SL-input-box">
+						<label>Confirm Password</label>
+						<input type="password" placeholder="Enter password again" name="confirmPassword" value={user.confirmPassword} onChange={handleChange} required />
+					</div>
+
+					<div className="SL-column">
+						<div className="SL-input-box">
+							<label>Phone Number</label>
+							<input type="number" placeholder="Enter phone number" name="phone" value={user.phone} onChange={handleChange} required />
+						</div>
+						<div className="SL-input-box">
+							<label>Birth Date</label>
+							<input type="date" placeholder="Enter birth date" name="date" value={user.date} onChange={handleChange} required />
+						</div>
+					</div>
+
+					<div className="SL-gender-box">
+						<h3>Gender</h3>
+						<div className="SL-gender-option">
+							<div className="SL-gender">
+								<input type="radio" id="check-male" name="gender"
+									value="M"
+									checked={gender === "M"}
+									onChange={handleRadio} />
+								<label htmlFor="check-male">Male</label>
+							</div>
+							<div className="SL-gender">
+								<input type="radio" id="check-female" name="gender"
+									value="F"
+									checked={gender === "F"}
+									onChange={handleRadio} />
+								<label htmlFor="check-female">Female</label>
+							</div>
+							<div className="SL-gender">
+								<input type="radio" id="check-other" name="gender"
+									value="P"
+									checked={gender === "P"}
+									onChange={handleRadio} />
+								<label htmlFor="check-other">prefer not to say</label>
+							</div>
+						</div>
+					</div>
+
+					<div className="SL-input-box">
+						<label>Social Secuirty Number</label>
+						<input type="number" placeholder="Enter SSN" name="ssn" value={user.ssn} onChange={handleChange} required />
+					</div>
+
+					<button onClick={handleSubmit}>Sign up</button>
+				</form>
+			</section>
+		</div>
 	);
 }
 

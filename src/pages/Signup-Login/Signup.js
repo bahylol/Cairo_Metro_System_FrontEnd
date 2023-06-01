03ad67734c9e3c84d5f4c82c9a1183ec3dc1aa22
bahylol <bahymohamed2010@gmail.com>
2023-06-01 13:54:03 +0300

    Update Signup.js

--- src/pages/Signup-Login/Signup.js
+++ src/pages/Signup-Login/Signup.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup-Login.css';
 
 function RegistrationForm() {
+	const [user, setUser] = useState({
+		username: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+		date: "",
+		phone: "",
+		ssn: "",
+	});
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		console.log(name);
+		console.log(value);
+		setUser(prevState => ({
+			...prevState,
+			[name]: value
+		}));
+	}
+	const [gender, setGender] = useState("");
+
+	const handleRadio = (event) => {
+		setGender(event.target.value);
+		console.log(setGender);
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if(user.password !==user.confirmPassword){
+			alert("passwords don't match");
+			return;
+		}
+		fetch('http://localhost:3000/api/v1/users/signup', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				"username": user.username,
+				"email": user.email,
+				"password": user.password,
+				"birthdate": user.date,
+				"gender": gender,
+				"phone": user.phone,
+				"ssn": user.ssn,
+				"usertype": "normal",
+				"userrole": "user"
+			})
+		})
+			.then(data => console.log(data))
+			.catch(error => console.error(error))
+	};
+
+
 	return (
 		<section className="container">
 			<header>Signup Form</header>
 			<form action="#" className="form">
+
 				<div className="column">
 					<div className="input-box">
 						<label>Full Name</label>
@@ -14,33 +68,33 @@ function RegistrationForm() {
 
 					<div className="input-box">
 						<label>Username</label>
-						<input type="text" placeholder="Enter Username" required />
+						<input type="text" placeholder="Enter Username" name="username" value={user.username} onChange={handleChange} required />
 					</div>
 				</div>
 
 				<div className="input-box">
 					<label>Email Address</label>
-					<input type="text" placeholder="Enter email address" required />
+					<input type="text" placeholder="Enter email address" name="email" value={user.email} onChange={handleChange} required />
 				</div>
 
 				<div className="input-box">
 					<label>Password</label>
-					<input type="password" placeholder="Enter password" required />
+					<input type="password" placeholder="Enter password" name="password" value={user.password} onChange={handleChange} required />
 				</div>
 
 				<div className="input-box">
 					<label>Confirm Password</label>
-					<input type="password" placeholder="Enter password again" required />
+					<input type="password" placeholder="Enter password again" name="confirmPassword" value={user.confirmPassword} onChange={handleChange} required />
 				</div>
 
 				<div className="column">
 					<div className="input-box">
 						<label>Phone Number</label>
-						<input type="number" placeholder="Enter phone number" required />
+						<input type="number" placeholder="Enter phone number" name="phone" value={user.phone} onChange={handleChange} required />
 					</div>
 					<div className="input-box">
 						<label>Birth Date</label>
-						<input type="date" placeholder="Enter birth date" required />
+						<input type="date" placeholder="Enter birth date" name="date" value={user.date} onChange={handleChange} required />
 					</div>
 				</div>
 
@@ -48,15 +102,24 @@ function RegistrationForm() {
 					<h3>Gender</h3>
 					<div className="gender-option">
 						<div className="gender">
-							<input type="radio" id="check-male" name="gender" defaultChecked />
+							<input type="radio" id="check-male" name="gender"
+								value="M"
+								checked={gender === "M"}
+								onChange={handleRadio} />
 							<label htmlFor="check-male">Male</label>
 						</div>
 						<div className="gender">
-							<input type="radio" id="check-female" name="gender" />
+							<input type="radio" id="check-female" name="gender"
+								value="F"
+								checked={gender === "F"}
+								onChange={handleRadio} />
 							<label htmlFor="check-female">Female</label>
 						</div>
 						<div className="gender">
-							<input type="radio" id="check-other" name="gender" />
+							<input type="radio" id="check-other" name="gender"
+								value="P"
+								checked={gender === "P"}
+								onChange={handleRadio} />
 							<label htmlFor="check-other">prefer not to say</label>
 						</div>
 					</div>
@@ -64,10 +127,10 @@ function RegistrationForm() {
 
 				<div className="input-box">
 					<label>Social Secuirty Number</label>
-					<input type="text" placeholder="Enter SSN" required />
+					<input type="number" placeholder="Enter SSN" name="ssn" value={user.ssn} onChange={handleChange} required />
 				</div>
 
-				<button>Sign up</button>
+				<button onClick={handleSubmit}>Sign up</button>
 			</form>
 		</section>
 	);

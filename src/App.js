c8e3d0506dd52fcf068ae0ad8d7e611134d2534f
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-06-01 10:51:31 +0300

    updated all css files, so that they dont mix together and result in a mess in all pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-// import Navbar from './pages/Navbar/Navbar2.js';
-// import Footer from './pages/Footer/Footer.js';
-// import Home from './pages/Home/Home.js';
-// import Signup from './pages/Signup-Login/Signup';
-// import Login from './pages/Signup-Login/Login';
-import Refund_Request_Page from './pages/Refund_Request/Refund_Request.js';
-// import modal from './pages/Refund_Request/refund_request_modal.js';
+// import './App.css';
+import Navbar from './pages/Navbar/Navbar2.js';
+import Footer from './pages/Footer/Footer.js';
+import Home from './pages/Home/Home.js';
+import Signup from './pages/Signup-Login/Signup';
+import Login from './pages/Signup-Login/Login';
+import RefundRequestPage from './pages/Refund_Request/Refund_Request.js';
 
 function App() {
 	return (
 		<div className="App">
 			<div className="Navbar">
+				<Navbar />
 				<div className="content">
-					<Refund_Request_Page />
+					<RefundRequestPage />
 				</div>
 			</div>
 		</div>

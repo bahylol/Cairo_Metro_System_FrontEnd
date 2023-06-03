6ad526e9815bbbb7906dccb34d09ba581a6471f8
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-06-03 20:08:35 +0300

    working on home page. added cards

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,15 +3,48 @@ import Footer from '../Footer/Footer.js';
 import './Home.css';
 import './style.css';
 import './articleCards.css';
+import './blogSlider.css';
+import { useEffect } from 'react';
 import image from '../../Assets/HomePageTrain.png';
 import cardPic1 from '../../Assets/cardPic1.jpg';
 import cardPic2 from '../../Assets/cardPic2.png';
 import cardPic3 from '../../Assets/cardPic3.jpg';
 import cardPic4 from '../../Assets/cardPic4.jpg';
+import crowd from '../../Assets/home_crowd.jpg';
+
+import Swiper from 'swiper';
 
 const Home = () => {
+	const currentDate = new Date();
+	const formattedCurrentDate = currentDate.toLocaleDateString('en-US', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	});
+	const dateParts = formattedCurrentDate.split(' ');
+	const day = parseInt(dateParts[1]);
+	const month = dateParts[0];
+
+	useEffect(() => {
+		const swiper = new Swiper('.blog-slider', {
+			spaceBetween: 30,
+			effect: 'fade',
+			loop: true,
+			mousewheel: {
+				invert: false,
+			},
+			pagination: {
+				el: '.blog-slider__pagination',
+				clickable: true,
+			},
+		});
+	}, []);
+
 	return (
 		<>
+			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</head>
 			<div className="Home">
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -31,8 +64,7 @@ const Home = () => {
 					<img className="home-train-image" src={image} alt="TRAIN-IMAGE" />
 				</div>
 				{/* --------------------------------------------------------------------------------- */}
-
-				<div className="home-get-ticket-container" style={{ height: '10vh' }}>
+				<div className="home-get-ticket-container" style={{ height: '40vh' }}>
 					<div className="home-pickup">
 						<p>Where do you want to go?</p>
 
@@ -60,10 +92,59 @@ const Home = () => {
 					</div>
 				</div>
 				{/* --------------------------------------------------------------------------------- */}
+				<div className="container mt-5">
+					<div className="row">
+						<div className="col-12">
+							<article className="blog-card">
+								<div className="blog-card__background">
+									<div className="card__background--wrapper">
+										<div
+											className="card__background--main"
+											style={{ backgroundImage: `url(${crowd})` }}
+										>
+											<div className="card__background--layer"></div>
+										</div>
+									</div>
+								</div>
+								<div className="blog-card__head">
+									<span className="date__box">
+										<span className="date__day">{day}</span>
+										<span className="date__month">{month}</span>
+									</span>
+								</div>
+								<div className="blog-card__info">
+									<h3>BECOME PART OF OUR AMAZING COMMUNITY TODAY</h3>
+									<p>
+										<a href="#" className="icon-link mr-3">
+											<i className="fa fa-pencil-square-o"></i> JOIN
+										</a>
+										<a href="#" className="icon-link">
+											<span className="larger-letter"> R</span>
+											ETRO
+											<span className="larger-letter">M</span>
+											ETRO
+										</a>
+									</p>
+									<p>
+										SIGN UP NOW for a seamless metro ticket booking experience. Join us
+										and enjoy convenience, personalized recommendations, and exclusive
+										benefits. Start your journey today!
+									</p>
+									<a href="#" className="btn btn--with-icon">
+										<i className="btn-icon fa fa-long-arrow-right"></i>
+										<span className="btn-text">SIGN UP</span>
+									</a>
+								</div>
+							</article>
+						</div>
+					</div>
+				</div>
+				{/* --------------------------------------------------------------------------------- */}
+
 				<div className="projcard-container">
 					<div className="projcard projcard-blue" style={{ marginLeft: '60px' }}>
 						<div className="projcard-innerbox">
-							<img className="projcard-img" src={cardPic1} />
+							<img className="projcard-img responsive-img" src={cardPic1} />
 							<div className="projcard-textbox">
 								<div className="projcard-title">Book Your Metro Tickets</div>
 								<div className="projcard-subtitle">Convenient and Hassle-Free Travel</div>
@@ -83,7 +164,7 @@ const Home = () => {
 
 					<div className="projcard projcard-red" style={{ marginLeft: '-60px' }}>
 						<div className="projcard-innerbox">
-							<img className="projcard-img" src={cardPic2} />
+							<img className="projcard-img responsive-img" src={cardPic2} />
 							<div className="projcard-textbox">
 								<div className="projcard-title">Manage Your Rides</div>
 								<div className="projcard-subtitle">Track, Modify, and Refund</div>
@@ -104,7 +185,7 @@ const Home = () => {
 
 					<div className="projcard projcard-green" style={{ marginLeft: '60px' }}>
 						<div className="projcard-innerbox">
-							<img className="projcard-img" src={cardPic3} />
+							<img className="projcard-img responsive-img" src={cardPic3} />
 							<div className="projcard-textbox">
 								<div className="projcard-title">Subscribe for Seamless Travel</div>
 								<div className="projcard-subtitle">Unlock Exclusive Benefits</div>
@@ -124,7 +205,7 @@ const Home = () => {
 
 					<div className="projcard projcard-customcolor" style={{ marginLeft: '-60px' }}>
 						<div className="projcard-innerbox">
-							<img className="projcard-img" src={cardPic4} />
+							<img className="projcard-img responsive-img" src={cardPic4} />
 							<div className="projcard-textbox">
 								<div className="projcard-title">Discover Metro Trivia</div>
 								<div className="projcard-subtitle">Fun and Fascinating Facts</div>

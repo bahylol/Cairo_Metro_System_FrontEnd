af2c1984c56459126034fac476b9ab0fe87b6813
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-06-03 18:00:50 +0300

    added logo and deleted some assets

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.js';
+import Navbar from './pages/Navbar/Navbar.js';
+import GeneralPage from './pages/General_Page/General_Page.js';
+import Home from './pages/Home/Home.js';
 import Signup from './pages/Signup-Login/Signup';
 import Login from './pages/Signup-Login/Login';
 import RefundRequestPage from './pages/Refund_Request/Refund_Request.js';
-import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import GetTicket from './pages/Get_Ticket/Get_Ticket.js';
+import Transactions from './pages/Transactions/Transactions.js';
+import ViewSubscription from './pages/View_Subscription/view_subscription.js';
+import Footer from './pages/Footer/Footer.js';
 
 const router = createBrowserRouter([
 	{
-		path: "/",
+		path: '/',
 		element: <App />,
 	},
 	{
-		path: "/login",
-		element: <Login />
+		path: '/login',
+		element: <Login />,
 	},
 	{
-		path: "/register",
-		element: <Signup />
+		path: '/register',
+		element: <Signup />,
 	},
 	{
-		path: "/test",
-		element: <RefundRequestPage />
-	}
+		path: '/test',
+		element: <RefundRequestPage />,
+	},
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-	<RouterProvider router={router} />
-);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
